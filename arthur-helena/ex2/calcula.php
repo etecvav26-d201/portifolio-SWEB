cf8d45f9f6073c0cc54c516495a7b1476778b21e
Arthur <arthurbatata1219@gmail.com>
2026-03-08 09:04:23 -0300

    lógica inicial para conversão e pequenas correções

--- arthur-helena/ex2/calcula.php
+++ arthur-helena/ex2/calcula.php
@@ -2,15 +2,20 @@
     $n1 = $_GET["n1"];
     $temp = $_GET["temp"];
     $resultadotemp = "";
+    $resultadoconversao = "";
 
-    if ($temp == "F" && $temp == "f") 
+    if ($temp == "F" || $temp == "f") 
     {
         $resultadotemp = "A temperatura registrada está em Fahreinheit.";
+        $resultadoconversao = 5/9*($n1 - 32);
     } 
-    else if ($temp =="C" && $temp == "c")
+    else if ($temp =="C" || $temp == "c")
     {
         $resultadotemp = "A temperatura registrada está em Celsius.";
+        $resultadoconversao = 9/5*($n1 + 32);
     }
+
+
 ?>
 
 <!DOCTYPE html>
