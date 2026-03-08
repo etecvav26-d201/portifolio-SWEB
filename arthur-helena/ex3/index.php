bfc75ed3ab32f39490dac5f468bbf39d9aba162e
Arthur <arthurbatata1219@gmail.com>
2026-03-08 09:43:35 -0300

    lógica inicial do index

--- arthur-helena/ex3/index.php
+++ arthur-helena/ex3/index.php
@@ -11,10 +11,27 @@ O programa deve mostrar o resultado da operação.
 <html lang="pt-br">
 <head>
   <meta charset="UTF-8">
-  <title></title>
+  <title>Calculadora Aritmética</title>
 </head>
 <body>
 
+    <h1>Calculadora Arimética.</h1>
+
+    <form action="calcula.php" method="get">
+        <label>Digite o primeiro número:</label>
+        <input type="number" name="n1" step="any" required>
+        <br><br>
+
+        <label>Digite qual operação deseja fazer (+, -, *, /)</label>
+        <input type="text" name="operacao" step="any" required>
+        <br><br>
+
+        <label>Digite o segundo número:</label>
+        <input type="number" name="n2" step="any" required>
+        <br><br>
+
+        <input type="submit" value="Calcular">
+    </form>
  
 </body>
 </html>
