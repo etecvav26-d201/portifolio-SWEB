d269c195f57e959307cb567b4719ad8a19e40817
Arthur <arthurbatata1219@gmail.com>
2026-03-08 08:24:19 -0300

    arquivo inicial html+php

---
+++ arthur-helena/ex1/calcula.php
@@ -0,0 +1,17 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <title></title>
+</head>
+
+<body>
+
+
+</body>
+
+</html>
---
+++ arthur-helena/ex1/index.php
@@ -0,0 +1,21 @@
+<!--
+Data: 08/03/2026
+Autor: Arthur Alexandre Dias Silva, Helena Bianquini Carriço
+Objetivo:
+
+Exercício 1 - Intervalo entre 100 e 200
+Faça um programa que receba um número e diga se este número está no intervalo entre 100 e 200.
+-->
+
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+  <meta charset="UTF-8">
+  <title></title>
+</head>
+<body>
+
+  
+
+</body>
+</html>
