ff009bab49810374f84e3ecb6ac88da618a9369d
Arthur <arthurbatata1219@gmail.com>
2026-03-08 10:03:46 -0300

    arquivos iniciais de html+php

---
+++ arthur-helena/ex4/index.php
@@ -0,0 +1,26 @@
+<!--
+Data: 08/03/2026
+Autor: Arthur Alexandre Dias Silva, Helena Bianquini Carriço
+Objetivo:
+
+Exercício 4 - Triângulo Numérico
+Leia um número n e imprima n linhas no seguinte formato (exemplo para n = 6):
+
+1
+1 2
+1 2 3
+1 2 3 4
+1 2 3 4 5
+1 2 3 4 5 6
+-->
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+  <meta charset="UTF-8">
+  <title></title>
+</head>
+<body>
+
+
+</body>
+</html>
