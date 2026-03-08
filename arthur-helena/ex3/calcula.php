511d6f991f99ccfbb89509b9774e4f47ae2613af
Arthur <arthurbatata1219@gmail.com>
2026-03-08 09:48:48 -0300

    lógica inicial da calculadora

--- arthur-helena/ex3/calcula.php
+++ arthur-helena/ex3/calcula.php
@@ -1,5 +1,24 @@
 <?php
-  
+    $n1 = $_GET["n1"];
+    $operacao = $_GET["operacao"];
+    $n2 = $_GET["n2"];
+    $resultado = "";
+
+    if ($operacao == "+") {
+        $resultado = $n1+$n2;
+    }
+    else if ($operacao == "-") {
+        $resultado = $n1-$n2;
+    } 
+    else if ($operacao == "*") {
+        $resultado = $n1*$n2;
+    }if ($operacao == "/") {
+        $resultado = $n1/$n2;
+    } else {
+        $resultado = "Caractere invalido para a operação."
+    }
+
+
 ?>
 
 <!DOCTYPE html>
