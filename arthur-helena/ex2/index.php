76d824425e1e097b22838255a120c340ad65f20b
Arthur <arthurbatata1219@gmail.com>
2026-03-08 08:50:02 -0300

    arquivo inicial html+php

---
+++ arthur-helena/ex2/index.php
@@ -0,0 +1,20 @@
+<!--
+Data: 08/03/2026
+Autor: Arthur Alexandre Dias Silva, Helena Bianquini Carriço
+Objetivo:
+
+Exercício 2 - Conversão de Temperatura
+Faça um programa que leia um caractere "F" ou "C", indicando se o valor informado está em Fahrenheit ou Celsius.
+Depois, o programa deve converter para a outra unidade.
+-->
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+  <meta charset="UTF-8">
+  <title></title>
+</head>
+<body>
+
+
+</body>
+</html>
