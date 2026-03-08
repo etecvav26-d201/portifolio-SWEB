cfe601c1e6c1e725cf154d1d7ea24aa0ceb58326
Arthur <arthurbatata1219@gmail.com>
2026-03-08 09:37:18 -0300

    paginas iniciais de html+php

---
+++ arthur-helena/ex3/index.php
@@ -0,0 +1,20 @@
+<!--
+Data: 08/03/2026
+Autor: Arthur Alexandre Dias Silva, Helena Bianquini Carriço
+Objetivo:
+
+Exercício 3 - Calculadora Aritmética
+Faça um programa que leia dois números e um operador ("+", "-", "*" ou "/").
+O programa deve mostrar o resultado da operação.
+-->
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+  <meta charset="UTF-8">
+  <title></title>
+</head>
+<body>
+
+ 
+</body>
+</html>
