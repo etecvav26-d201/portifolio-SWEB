595f246b0db8a8cd00ae52fb0f7a74f11b17059a
Arthur <arthurbatata1219@gmail.com>
2026-03-08 10:32:58 -0300

    arquivos iniciais html+php

---
+++ arthur-helena/ex5/index.php
@@ -0,0 +1,19 @@
+<!--
+Data: 08/03/2026
+Autor: Arthur Alexandre Dias Silva, Helena Bianquini Carriço
+Objetivo:
+
+Exercício 5 - Somatório de Fatoriais
+Leia 5 números inteiros e mostre a soma dos fatoriais desses números.
+-->
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+  <meta charset="UTF-8">
+  <title></title>
+</head>
+<body>
+
+  
+</body>
+</html>
