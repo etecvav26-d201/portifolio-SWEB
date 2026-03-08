56d0731dda8e78ebb8bac1605ad1fd63d807928e
Arthur <arthurbatata1219@gmail.com>
2026-03-08 08:59:57 -0300

    lógica inicial para verificar temperatura

--- arthur-helena/ex2/calcula.php
+++ arthur-helena/ex2/calcula.php
@@ -1,5 +1,16 @@
 <?php
+    $n1 = $_GET["n1"];
+    $temp = $_GET["temp"];
+    $resultadotemp = "";
 
+    if ($temp == "F" && $temp == "f") 
+    {
+        $resultadotemp = "A temperatura registrada está em Fahreinheit.";
+    } 
+    else if ($temp =="C" && $temp == "c")
+    {
+        $resultadotemp = "A temperatura registrada está em Celsius.";
+    }
 ?>
 
 <!DOCTYPE html>
