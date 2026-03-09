3c8dbbb2fc74e36300333eda91bd03163d75140c
helenoka <helenacarrico12@gmail.com>
2026-03-09 11:00:29 -0300

    arquivos

---
+++ arthur-helena/ex9/index.php
@@ -0,0 +1,31 @@
+<!--
+Data: 09/03/2026
+Autor: Arthur Alexandre Dias Silva, Helena Bianquini Carriço
+Objetivo:
+
+Exercício 9 - Média Aritmética com Função
+Crie uma função:
+function media($v)
+Que receba uma lista de números reais e retorne a média aritmética.
+-->
+
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+  <meta charset="UTF-8">
+  <title>Média Aritmética com Função</title>
+</head>
+<body>
+
+    <h1>Média Aritmética com Função</h1>
+
+    <form action="calcula.php" method="get">
+        <label>Digite um número real:</label>
+        <input type="number" name="n1" step="any" required>
+        <br><br>
+
+        <input type="submit" value="Calcular">
+    </form>
+
+</body>
+</html>
