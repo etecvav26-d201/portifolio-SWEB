f70d768bde39fd820c78220d991e2874945e9701
helenoka <helenacarrico12@gmail.com>
2026-03-09 09:10:58 -0300

    arquivos pro trabalho

---
+++ arthur-helena/ex6/index.php
@@ -0,0 +1,26 @@
+<!--
+Data: 09/03/2026
+Autor: Arthur Alexandre Dias Silva, Helena Bianquini Carriço
+Objetivo:
+
+Exercício 6 - Série de Fibonacci
+Leia um número n e mostre os n primeiros termos da sequência de Fibonacci.
+
+Exemplo:
+n = 12
+Resultado:
+0 1 1 2 3 5 8 13 21 34 55 89
+-->
+
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+  <meta charset="UTF-8">
+  <title></title>
+</head>
+<body>
+
+
+
+</body>
+</html>
