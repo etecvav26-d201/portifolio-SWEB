25e4b02457b5f343243e92008e2930b5d1e70c0e
helenoka <helenacarrico12@gmail.com>
2026-03-09 09:45:59 -0300

    arquivos

---
+++ arthur-helena/ex8/index.php
@@ -0,0 +1,8 @@
+<!--
+Data: 09/03/2026
+Autor: Arthur Alexandre Dias Silva, Helena Bianquini Carriço
+Objetivo:
+
+Exercício 8 - Soma de 0 até N com Função
+Crie uma função soma(n) que receba um número inteiro e retorne a soma de 0 até esse número.
+-->
