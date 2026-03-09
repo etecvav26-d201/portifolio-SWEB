7c4eae5f362fc3000bf1a28330cc40989153fbdf
helenoka <helenacarrico12@gmail.com>
2026-03-09 09:47:03 -0300

    formulario

--- arthur-helena/ex8/index.php
+++ arthur-helena/ex8/index.php
@@ -6,3 +6,24 @@ Objetivo:
 Exercício 8 - Soma de 0 até N com Função
 Crie uma função soma(n) que receba um número inteiro e retorne a soma de 0 até esse número.
 -->
+
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+  <meta charset="UTF-8">
+  <title>Soma de 0 até N com função</title>
+</head>
+<body>
+
+    <h1>Soma de 0 até N com função</h1>
+
+    <form action="calcula.php" method="get">
+    <label>Digite um número:</label>
+    <input type="number" name="n1" step="any" required>
+    <br><br>
+
+    <input type="submit" value="Calcular">
+    </form>
+
+</body>
+</html>
