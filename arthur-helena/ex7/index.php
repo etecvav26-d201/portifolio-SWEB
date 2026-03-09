eda9afa515667def8b9b46dd5aae63b322a668e5
helenoka <helenacarrico12@gmail.com>
2026-03-09 09:27:52 -0300

    formularios

--- arthur-helena/ex7/index.php
+++ arthur-helena/ex7/index.php
@@ -8,4 +8,61 @@ Leia 8 números inteiros e separe em dois vetores:
 
 Um vetor com números positivos
 Um vetor com números negativos
--->
+--> 
+
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+  <meta charset="UTF-8">
+  <title>Separar positivos e negativos</title>
+</head>
+<body>
+
+    <h1>eparar positivos e negativos</h1>
+
+    <form action="calcula.php" method="get">
+    <label>Digite o primeiro número:</label>
+    <input type="number" name="n1" step="any" required>
+    <br><br>
+
+    <form action="calcula.php" method="get">
+    <label>Digite o segundo número:</label>
+    <input type="number" name="n2" step="any" required>
+    <br><br>
+
+    <form action="calcula.php" method="get">
+    <label>Digite o terceiro número:</label>
+    <input type="number" name="n3" step="any" required>
+    <br><br>
+
+    <form action="calcula.php" method="get">
+    <label>Digite o quarto número:</label>
+    <input type="number" name="n4" step="any" required>
+    <br><br>
+
+    <form action="calcula.php" method="get">
+    <label>Digite o quinto número:</label>
+    <input type="number" name="n5" step="any" required>
+    <br><br>
+
+    <form action="calcula.php" method="get">
+    <label>Digite o sexto número:</label>
+    <input type="number" name="n6" step="any" required>
+    <br><br>
+
+    <form action="calcula.php" method="get">
+    <label>Digite o sétimo número:</label>
+    <input type="number" name="n7" step="any" required>
+    <br><br>
+
+    <form action="calcula.php" method="get">
+    <label>Digite o oitavo número:</label>
+    <input type="number" name="n8" step="any" required>
+    <br><br>
+
+
+    <input type="submit" value="Calcular">
+    </form>
+
+</body>
+</html>
