d71019beac667b6da37fd5cd1894b7983516c545
helenoka <helenacarrico12@gmail.com>
2026-03-09 09:25:20 -0300

    arquivos

---
+++ arthur-helena/ex7/index.php
@@ -0,0 +1,11 @@
+<!--
+Data: 09/03/2026
+Autor: Arthur Alexandre Dias Silva, Helena Bianquini Carriço
+Objetivo:
+
+Exercício 7 - Separar Positivos e Negativos
+Leia 8 números inteiros e separe em dois vetores:
+
+Um vetor com números positivos
+Um vetor com números negativos
+-->
