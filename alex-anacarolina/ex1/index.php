9f8e0b399c362625f88a07c640c2fe55fcc21307
Arthur <arthurbatata1219@gmail.com>
2026-03-19 18:03:10 -0300

    codigo

--- alex-anacarolina/ex1/index.php
+++ alex-anacarolina/ex1/index.php
@@ -6,3 +6,15 @@ Objetivo:
 Exercício 1 - Verificar intervalo
 Criar um programa que receba um número e informe se ele está entre 100 e 200.
 -->
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+  <meta charset="UTF-8">
+  <title>Verificação de Intervalo</title>
+</head>
+<body>
+
+  <h1>Verificar números entre 100 e entre 200</h1>
+
+</body>
+</html>
