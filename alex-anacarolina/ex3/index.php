c1e5b69b99553b971f5a2529f85de4e775c8ca57
Ana <anacarolinabernalsantos@gmail.com>
2026-03-19 18:12:48 -0300

    html

--- alex-anacarolina/ex3/index.php
+++ alex-anacarolina/ex3/index.php
@@ -7,3 +7,15 @@ Exercício 3 - Calculadora
 Criar um programa que receba dois números e um operador matemático
 (+, -, * ou /) e mostre o resultado da operação escolhida.
 -->
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+  <meta charset="UTF-8">
+  <title>Calculadora Aritmética</title>
+</head>
+<body>
+
+    <h1>Calculadora Aritmética</h1>
+ 
+</body>
+</html>
