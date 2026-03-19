03ff0cdf39e56a1242be3bc08a20f3ab5926abb0
Arthur <arthurbatata1219@gmail.com>
2026-03-19 18:02:25 -0300

    instruções

---
+++ alex-anacarolina/ex1/index.php
@@ -0,0 +1,8 @@
+<!--
+Data: 16/03/2026
+Autores: Alex dos Santos Apolinario e Ana Carolina Bernal Santos
+
+Objetivo:
+Exercício 1 - Verificar intervalo
+Criar um programa que receba um número e informe se ele está entre 100 e 200.
+-->
