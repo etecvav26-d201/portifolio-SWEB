85c3a1658d7cf46ca041713fcbf0c4fc807048b8
Ana <anacarolinabernalsantos@gmail.com>
2026-03-19 18:12:57 -0300

    formulario

--- alex-anacarolina/ex3/index.php
+++ alex-anacarolina/ex3/index.php
@@ -16,6 +16,27 @@ Criar um programa que receba dois números e um operador matemático
 <body>
 
     <h1>Calculadora Aritmética</h1>
+
+    <form action="calcula.php" method="get">
+
+        <label for="num1">Informe o primeiro número:</label>
+        <input type="number" id="num1" name="n1" step="any" required>
+        
+        <br><br>
+
+        <label for="op">Digite a operação (+, -, *, /):</label>
+        <input type="text" id="op" name="operacao" maxlength="1" required>
+        
+        <br><br>
+
+        <label for="num2">Informe o segundo número:</label>
+        <input type="number" id="num2" name="n2" step="any" required>
+        
+        <br><br>
+
+        <input type="submit" value="Calcular resultado">
+
+    </form>
  
 </body>
 </html>
