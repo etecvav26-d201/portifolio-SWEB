6bd53611c180530098e808d4d13c886d2aace865
Ana <anacarolinabernalsantos@gmail.com>
2026-03-19 18:19:32 -0300

    exercicio

---
+++ alex-anacarolina/ex5/index.php
@@ -0,0 +1,8 @@
+<!--
+Data: 16/03/2026
+Autores: Ana Carolina Bernal Santos e Alex dos Santos Apolinario
+
+Objetivo:
+Exercício 5 - Somatório de Fatoriais
+Criar um programa que receba 5 números inteiros e mostre a soma dos fatoriais desses números.
+-->
