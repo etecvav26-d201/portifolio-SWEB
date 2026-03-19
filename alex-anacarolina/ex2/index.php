aed814ed7cac2122da3213ac385528a0f4ae7ae6
Ana <anacarolinabernalsantos@gmail.com>
2026-03-19 18:08:40 -0300

    exercicio

---
+++ alex-anacarolina/ex2/index.php
@@ -0,0 +1,9 @@
+<!--
+Data: 12/03/2026
+Autores: Alex dos Santos Apolinario e Ana Carolina Bernal Santos
+
+Objetivo:
+Exercício 2 - Converter Temperatura
+Criar um programa que receba um valor de temperatura e uma letra (F ou C),
+indicando a unidade. Em seguida, realizar a conversão para a outra escala.
+-->
