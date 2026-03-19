654a0703e00294327bb9f410fefc99a9e75de897
Ana <anacarolinabernalsantos@gmail.com>
2026-03-19 18:09:19 -0300

    html

--- alex-anacarolina/ex2/index.php
+++ alex-anacarolina/ex2/index.php
@@ -7,3 +7,15 @@ Exercício 2 - Converter Temperatura
 Criar um programa que receba um valor de temperatura e uma letra (F ou C),
 indicando a unidade. Em seguida, realizar a conversão para a outra escala.
 -->
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+  <meta charset="UTF-8">
+  <title>Conversor de Temperaturas</title>
+</head>
+<body>
+
+    <h1>Conversor de Temperaturas</h1>
+
+</body>
+</html>
