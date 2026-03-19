f6072336eab5302414801d277f7ff8fbe5296f3e
Ana <anacarolinabernalsantos@gmail.com>
2026-03-19 18:15:55 -0300

    exercicio

---
+++ alex-anacarolina/ex4/index.php
@@ -0,0 +1,18 @@
+<!--
+Data: 12/03/2026
+Autores: Ana Carolina Bernal Santos e Alex dos Santos Apolinario
+
+Objetivo:
+Exercício 4 - Formação de Triângulo Numérico
+Criar um programa que leia um número n e mostre um triângulo
+com os números de 1 até n em cada linha.
+
+Exemplo para n = 6:
+
+1
+1 2
+1 2 3
+1 2 3 4
+1 2 3 4 5
+1 2 3 4 5 6
+-->
