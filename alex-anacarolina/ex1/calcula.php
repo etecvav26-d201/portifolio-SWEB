cd285bf8a0156842a7a7c787114d82c52bae899f
Ana <anacarolinabernalsantos@gmail.com>
2026-03-19 18:06:50 -0300

    html

--- alex-anacarolina/ex1/calcula.php
+++ alex-anacarolina/ex1/calcula.php
@@ -8,3 +8,20 @@
         $resultado = "não está entre 100 e 200.";
     }
 ?>
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <title>Resultado</title>
+</head>
+
+<body>
+
+    <p>
+        <?php 
+            echo "O número " . $valor . " " . $resultado; 
+        ?>
+    </p>
+
+</body>
+</html>
