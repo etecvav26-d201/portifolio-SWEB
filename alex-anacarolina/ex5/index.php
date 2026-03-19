eb6334d63782606158b121c201d36849a7759222
Ana <anacarolinabernalsantos@gmail.com>
2026-03-19 18:20:28 -0300

    html codigo

--- alex-anacarolina/ex5/index.php
+++ alex-anacarolina/ex5/index.php
@@ -6,3 +6,15 @@ Objetivo:
 Exercício 5 - Somatório de Fatoriais
 Criar um programa que receba 5 números inteiros e mostre a soma dos fatoriais desses números.
 -->
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+  <meta charset="UTF-8">
+  <title>Somatório de Fatoriais</title>
+</head>
+<body>
+
+  <h1>Somatório dos Fatoriais</h1>
+
+</body>
+</html>
