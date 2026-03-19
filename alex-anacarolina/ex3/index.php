0823b08849481f8729af46eda213ac035c14e224
Ana <anacarolinabernalsantos@gmail.com>
2026-03-19 18:12:00 -0300

    exercicio

---
+++ alex-anacarolina/ex3/index.php
@@ -0,0 +1,9 @@
+<!--
+Data: 12/03/2026
+Autores: Alex dos Santos Apolinario e Ana Carolina Bernal Santos
+
+Objetivo:
+Exercício 3 - Calculadora
+Criar um programa que receba dois números e um operador matemático
+(+, -, * ou /) e mostre o resultado da operação escolhida.
+-->
