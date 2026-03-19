30929c88696c4654282d3612bb0065e45ef074a6
Arthur <arthurbatata1219@gmail.com>
2026-03-19 18:03:40 -0300

    formulario

--- alex-anacarolina/ex1/index.php
+++ alex-anacarolina/ex1/index.php
@@ -16,5 +16,14 @@ Criar um programa que receba um número e informe se ele está entre 100 e 200.
 
   <h1>Verificar números entre 100 e entre 200</h1>
 
+  <form action="calcula.php" method="get">
+    <label for="numero">Escreva um numero:</label>
+    <input type="number" id="numero" name="numero" step="any" required>
+    
+    <br><br>
+
+    <input type="submit" value="Verificar">
+  </form>
+
 </body>
 </html>
