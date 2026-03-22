89c69bcff096322fef4952ed6d960742c1d39863
Alex <alexsantosapolinario@gmail.com>
2026-03-22 16:29:10 -0300

    html com formulario

--- 1-BIM-FundamentosPHP/alex-anacarolina/ex6/index.php
+++ 1-BIM-FundamentosPHP/alex-anacarolina/ex6/index.php
@@ -11,4 +11,24 @@ n = 12
 Resultado:
 0 1 1 2 3 5 8 13 21 34 55 89
 -->
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+  <meta charset="UTF-8">
+  <title>Fibonacci</title>
+</head>
+<body>
+
+    <h1>Fibonacci</h1>
+
+    <form action="calcula.php" method="get">
+    <label>Digite o número:</label>
+    <input type="number" name="n" step="any" required>
+    <br><br>
+
+    <input type="submit" value="Calcular">
+    </form>
+
+</body>
+</html>
 
