4d91945f28d3d15cbdfefa82671721c05e422442
Alex <alexsantosapolinario@gmail.com>
2026-03-22 16:24:22 -0300

    documentação

---
+++ 1-BIM-FundamentosPHP/alex-anacarolina/ex10/index.php
@@ -0,0 +1,13 @@
+<!--
+Data: 09/03/2026
+Autor: Alex dos Santos Apolinario, Ana Carolina Bernal Santos
+Objetivo:
+
+Exercício 10 - Ano Bissexto
+Leia um ano e informe se ele é bissexto.
+
+Um ano é bissexto se:
+É múltiplo de 400
+ou
+É múltiplo de 4 e não é múltiplo de 100
+-->
