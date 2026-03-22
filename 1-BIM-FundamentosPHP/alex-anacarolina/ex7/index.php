8f8ac3179726a786d8d7de34f3585387d6d580f9
Alex <alexsantosapolinario@gmail.com>
2026-03-22 16:15:18 -0300

    html com formulario

--- 1-BIM-FundamentosPHP/alex-anacarolina/ex7/index.php
+++ 1-BIM-FundamentosPHP/alex-anacarolina/ex7/index.php
@@ -9,3 +9,24 @@ Leia 8 números inteiros e separe em dois vetores:
 Um vetor com números positivos
 Um vetor com números negativos
 --> 
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+  <meta charset="UTF-8">
+  <title>Separar números positivos dos negativos</title>
+</head>
+<body>
+
+    <h1>Separar números positivos dos negativos</h1>
+
+    <form action="calcula.php" method="get">
+        <?php for ($i = 1; $i <= 8; $i++): ?>
+            <label for="num<?php echo $i; ?>">Número <?php echo $i; ?>:</label>
+            <input type="number" name="n<?php echo $i; ?>" required><br>
+        <?php endfor; ?>
+
+        <input type="submit" value="Calcular">
+    </form>
+
+</body>
+</html>
