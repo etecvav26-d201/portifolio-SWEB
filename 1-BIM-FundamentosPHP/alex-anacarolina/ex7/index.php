a5fe23c3f0bf36cdfb755f2201a970555937bc7d
Alex <alexsantosapolinario@gmail.com>
2026-03-22 16:12:41 -0300

    documentação

---
+++ 1-BIM-FundamentosPHP/alex-anacarolina/ex7/index.php
@@ -0,0 +1,11 @@
+<!--
+Data: 09/03/2026
+Autor: Alex dos Santos Apolinario, Ana Carolina Bernal Santos
+Objetivo:
+
+Exercício 7 - Separar Positivos e Negativos
+Leia 8 números inteiros e separe em dois vetores:
+
+Um vetor com números positivos
+Um vetor com números negativos
+--> 
