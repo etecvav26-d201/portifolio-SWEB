eb2df36eaabb9556ed84dfbc5e4cd5cc2dafdd8d
Alex <alexsantosapolinario@gmail.com>
2026-03-22 16:22:38 -0300

    html com formulario

--- 1-BIM-FundamentosPHP/alex-anacarolina/ex9/index.php
+++ 1-BIM-FundamentosPHP/alex-anacarolina/ex9/index.php
@@ -8,3 +8,23 @@ Crie uma função:
 function media($v)
 Que receba uma lista de números reais e retorne a média aritmética.
 -->
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+  <meta charset="UTF-8">
+  <title>Média Aritmética com Função</title>
+</head>
+<body>
+
+    <h1>Média Aritmética com Função</h1>
+
+    <form action="calcula.php" method="get">
+        <label>Digite um número:</label>
+        <input type="number" name="n" step="any" required>
+        <br><br>
+
+        <input type="submit" value="Calcular">
+    </form>
+
+</body>
+</html>
