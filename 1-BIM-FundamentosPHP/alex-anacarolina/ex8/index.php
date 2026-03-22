b12c6debf969f5291f533c2b11de564dc343d7fa
Alex <alexsantosapolinario@gmail.com>
2026-03-22 16:18:22 -0300

    documentação

---
+++ 1-BIM-FundamentosPHP/alex-anacarolina/ex8/index.php
@@ -0,0 +1,8 @@
+<!--
+Data: 09/03/2026
+Autor: Alex Santos Apolinario, Ana Carolina Bernal Santos
+Objetivo:
+
+Exercício 8 - Soma de 0 até N com Função
+Crie uma função soma(n) que receba um número inteiro e retorne a soma de 0 até esse número.
+-->
