b8a4df3810a553fc4a50b4ae8b10e8a93b7327ff
Alex <alexsantosapolinario@gmail.com>
2026-03-22 16:22:11 -0300

    documentação

---
+++ 1-BIM-FundamentosPHP/alex-anacarolina/ex9/index.php
@@ -0,0 +1,10 @@
+<!--
+Data: 09/03/2026
+Autor: Alex dos Santos Apolinario, Ana Carolina Bernal Santos
+Objetivo:
+
+Exercício 9 - Média Aritmética com Função
+Crie uma função:
+function media($v)
+Que receba uma lista de números reais e retorne a média aritmética.
+-->
