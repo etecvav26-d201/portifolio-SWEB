5c8c9c25acfa5740f646fd2666536c2072629f8b
Arthur <arthurbatata1219@gmail.com>
2026-05-11 12:18:23 -0300

    Código para mostrar erros de pdo

--- 2-BIM-AtividadeM1/config/conexao.php
+++ 2-BIM-AtividadeM1/config/conexao.php
@@ -1,8 +1,24 @@
 <?php
 
-$host = "localhost";
-$banco = "farmacia_vav";
-$usuario = "root";
-$senha = "";
+    $host = "localhost";
+    $banco = "farmacia_vav";
+    $usuario = "root";
+    $senha = "";
+
+    try {
+
+        $conexao = new PDO(
+            "mysql:host=$host;dbname=$banco;charset=utf8",
+            $usuario,
+            $senha
+        );
+    
+        $conexao->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    
+    } catch (PDOException $erro) {
+    
+        die("Erro na conexão com o banco de dados: " . $erro->getMessage());
+    
+    }
 
 ?>
