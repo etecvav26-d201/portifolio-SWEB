70f3b2e7fb7520e4aa471d9a171c713106d7388a
Ana <anacarolinabernalsantos@gmail.com>
2026-05-11 14:33:08 -0300

    Parte pra atualizar o produto

--- 2-BIM-AtividadeM1/editar.php
+++ 2-BIM-AtividadeM1/editar.php
@@ -14,3 +14,31 @@ $comando->execute([
 
 $produto = $comando->fetch(PDO::FETCH_ASSOC);
 
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+
+    $nome = $_POST['nome'];
+    $fabricante = $_POST['fabricante'];
+    $preco = $_POST['preco'];
+    $estoque = $_POST['estoque'];
+
+    $sql = "UPDATE produtos SET
+            nome = :nome,
+            fabricante = :fabricante,
+            preco = :preco,
+            estoque = :estoque
+            WHERE id = :id";
+
+    $comando = $conexao->prepare($sql);
+
+    $comando->execute([
+        ':nome' => $nome,
+        ':fabricante' => $fabricante,
+        ':preco' => $preco,
+        ':estoque' => $estoque,
+        ':id' => $id
+    ]);
+
+    header('Location: index.php');
+
+
