506f134051737a9385dbb78f5630189228c65290
Alex <alexsantosapolinario@gmail.com>
2026-05-11 12:23:05 -0300

    Adicionado código PHP para o cadastro

---
+++ 2-BIM-AtividadeM1/cadastro.php
@@ -0,0 +1,29 @@
+<?php
+
+require_once 'config/conexao.php';
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+
+    $nome = $_POST['nome'];
+    $fabricante = $_POST['fabricante'];
+    $preco = $_POST['preco'];
+    $estoque = $_POST['estoque'];
+
+    $sql = "INSERT INTO produtos 
+            (nome, fabricante, preco, estoque)
+            VALUES
+            (:nome, :fabricante, :preco, :estoque)";
+
+    $comando = $conexao->prepare($sql);
+
+    $comando->execute([
+        ':nome' => $nome,
+        ':fabricante' => $fabricante,
+        ':preco' => $preco,
+        ':estoque' => $estoque
+    ]);
+
+    echo "Produto cadastrado com sucesso!";
+}
+
+?>
