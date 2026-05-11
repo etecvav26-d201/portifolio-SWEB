00c9f3616f2b07f467aa876369498669f157ed2d
Arthur <arthurbatata1219@gmail.com>
2026-05-11 12:32:09 -0300

    Código .html base

--- 2-BIM-AtividadeM1/index.php
+++ 2-BIM-AtividadeM1/index.php
@@ -9,3 +9,17 @@
     $produtos = $resultado->fetchAll(PDO::FETCH_ASSOC);
 
 ?>
+
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Listagem dos produtos</title>
+</head>
+<body>
+
+
+
+</body>
+</html>
